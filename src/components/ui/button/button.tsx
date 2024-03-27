e81df6ceab5ad291108e78999436cf8dc83f82a2
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-03-27 18:17:26 +0300

    add button component

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,31 @@
+import { ComponentPropsWithoutRef, ElementRef, ElementType, ForwardedRef, forwardRef } from 'react'
+
+import { ButtonVariant } from '@/common'
+
+import s from './button.module.scss'
+
+type Props<T extends ElementType> = {
+  as?: T
+  variant?: keyof typeof ButtonVariant
+}
+
+const ButtonPolymorph = <T extends ElementType = 'button'>(
+  props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props<T>>,
+  ref: ElementRef<T>
+) => {
+  const { as: Tag = 'button', className, variant = ButtonVariant.primary, ...rest } = props
+
+  const buttonClassName = `${s.button} ${s[String(variant)]}`
+
+  return (
+    // @ts-expect-error TS2322
+    <Tag className={buttonClassName} ref={ref} {...rest} />
+  )
+}
+
+export const Button = forwardRef(ButtonPolymorph) as <T extends ElementType = 'button'>(
+  props: Props<T> &
+    Omit<ComponentPropsWithoutRef<T>, keyof Props<T>> & {
+      ref?: ForwardedRef<ElementRef<T>>
+    }
+) => ReturnType<typeof ButtonPolymorph>
