9650c8899c867fcd5c117ac65c64808502f5d941
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-03-29 12:36:00 +0300

    correct typography component (add hook and calculate with for svg)

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,6 +1,7 @@
-import { ComponentPropsWithoutRef, ElementRef, ElementType, ForwardedRef, forwardRef } from 'react'
+import { ComponentPropsWithoutRef, ElementType } from 'react'
 
 import { TypographyVariant } from '@/common'
+import { useTypography } from '@/components'
 
 import s from './typography.module.scss'
 
@@ -9,9 +10,8 @@ type Props<T> = {
   isLine?: boolean
   variant?: keyof typeof TypographyVariant
 }
-const TypographyPolymorph = <T extends ElementType = 'p'>(
-  props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props<T>>,
-  ref: ElementRef<T>
+export const Typography = <T extends ElementType = 'p'>(
+  props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props<T>>
 ) => {
   const {
     as: Tag = 'p',
@@ -24,34 +24,17 @@ const TypographyPolymorph = <T extends ElementType = 'p'>(
 
   const typographyClassName = `${isLine && s.line} ${s[String(variant)]} ${className}`
 
-  const svgWidth = 500 // Замените это значением ширины вашего SVG
-  const svgHeight = 50 // Замените это значением высоты вашего SVG
-
-  const startX = 0
-  const startY = 15 //96
-  const control1X = svgWidth * 0.25
-  const control1Y = 0 //81
-  const control2X = svgWidth * 0.5
-  const control2Y = 9 //90
-  const endX = svgWidth
-  const endY = 19 //99
-
-  const pathD = `M${startX},${startY} C${control1X},${control1Y} ${control2X},${control2Y} ${endX},${endY}`
+  const { path, svgWidth, typographyRef } = useTypography()
 
   return (
     // @ts-expect-error TS2322
-    <Tag className={typographyClassName} ref={ref} {...rest}>
+    <Tag className={typographyClassName} ref={typographyRef} {...rest}>
       {children}
       {isLine && (
-        <svg height={svgHeight} width={svgWidth}>
-          <path d={pathD} fill={'none'} strokeLinecap={'round'} strokeWidth={6} />
+        <svg height={30} viewBox={`0 0 ${svgWidth} 32`} width={svgWidth}>
+          <path d={path} fill={'none'} strokeLinecap={'round'} strokeWidth={6} />
         </svg>
       )}
     </Tag>
   )
 }
-
-export const Typography = forwardRef(TypographyPolymorph) as <T extends ElementType = 'button'>(
-  props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props<T>>,
-  ref?: ForwardedRef<ElementRef<T>>
-) => ReturnType<typeof TypographyPolymorph>
