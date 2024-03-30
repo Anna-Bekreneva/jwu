95d9400d43f173612dc1fd45a78f01a2fa53671d
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-03-30 17:33:28 +0300

    add field component

---
+++ src/components/ui/field/field.tsx
@@ -0,0 +1,39 @@
+import { ChangeEvent, ComponentPropsWithoutRef, ElementType } from 'react'
+
+import { TypographyVariant } from '@/common'
+import { Typography } from '@/components'
+
+import s from './field.module.scss'
+
+type Props<T extends ElementType> = {
+  as?: T
+  id?: string
+  label?: string
+  onValueChange: (value: string) => void
+}
+export const Field = <T extends ElementType = 'input'>(
+  props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props<T>>
+) => {
+  const { as: Tag = 'input', id, label, onValueChange, ...rest } = props
+
+  return (
+    <div className={s.box}>
+      {label && (
+        <Typography
+          as={'label'}
+          className={s.label}
+          htmlFor={id}
+          variant={TypographyVariant.caption2}
+        >
+          {label}
+        </Typography>
+      )}
+      <Tag
+        className={`${s.field} ${Tag === 'textarea' && s.textarea}`}
+        id={id}
+        onChange={e => onValueChange(e.currentTarget.value)}
+        {...rest}
+      ></Tag>
+    </div>
+  )
+}
