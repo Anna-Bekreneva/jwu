b9e7a5cde141bdfb754e572e41dbe01e3fcd0fff
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-03-31 23:08:52 +0300

    add picture component

---
+++ src/components/ui/picture/picture.tsx
@@ -0,0 +1,19 @@
+import { ComponentPropsWithoutRef, FC } from 'react'
+
+type Props = {
+  alt: string
+  avif: string
+  src: string
+  webp: string
+}
+
+export type PicturePropsType = Props & Omit<ComponentPropsWithoutRef<'img'>, keyof Props>
+export const Picture: FC<PicturePropsType> = ({ alt, avif, className, src, webp, ...rest }) => {
+  return (
+    <picture>
+      <source srcSet={avif} type={'image/avif'} />
+      <source srcSet={webp} type={'image/webp'} />
+      <img alt={alt} src={src} {...rest} />
+    </picture>
+  )
+}
