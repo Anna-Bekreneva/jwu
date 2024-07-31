13e03de4e7ec418346156f216f280be872c7bd59
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-07-31 20:10:35 +0300

    refactor and start privacy-policy

---
+++ src/pages/privacy-policy-page/privacy-policy-page.tsx
@@ -0,0 +1,5 @@
+import { PrivacyPolicyContent } from './blocks'
+
+export const PrivacyPolicyPage = () => {
+  return <PrivacyPolicyContent />
+}
---
+++ src/utils/constants/base.tsx
@@ -0,0 +1,23 @@
+export const APP_ROUTES = {
+  home: '/',
+  privacyPolicy: '/privacy-policy',
+}
+
+// export const CONTACTS = {
+//   email: {
+//     href: 'mailto:info@kindheartsfund.com',
+//     label: 'info@kindheartsfund.com',
+//   },
+//   instagram: {
+//     href: 'https://www.instagram.com/kindheartsfund?igsh=NnoxcjZ0dHV6N2d6',
+//     label: '@kindheartsfund',
+//   },
+//   phone: {
+//     href: 'tel:+12012280088',
+//     label: '+1 (201) 2280088',
+//   },
+//   webSite: {
+//     href: 'https://kind-hearts.vercel.app',
+//     label: 'https://kind-hearts.vercel.app',
+//   },
+// }
